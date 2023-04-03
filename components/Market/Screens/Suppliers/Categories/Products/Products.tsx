e406f38f95fd951b2fa65fa7181879d9fbab6efa
H3XG0D <99123233+H3XG0D@users.noreply.github.com>
2023-04-03 14:02:53 +0900

    Add Products list

--- components/Market/Screens/Suppliers/Categories/Products/Products.tsx
+++ components/Market/Screens/Suppliers/Categories/Products/Products.tsx
@@ -8,12 +8,12 @@ import {getCategoriesInfo, getProductsInfo} from '../../../../../../api/api';
 import {useRoute} from '@react-navigation/native';
 import {ScrollView} from 'react-native-gesture-handler';
 
-const Categories: React.FunctionComponent<IStackScreenProps> = props => {
+const Products: React.FunctionComponent<IStackScreenProps> = props => {
   const {navigation} = props;
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: content.name,
+      headerTitle: category.name,
       headerTitleStyle: {fontSize: 20},
     });
   }, [navigation]);
@@ -44,10 +44,122 @@ const Categories: React.FunctionComponent<IStackScreenProps> = props => {
   return (
     <View style={{height: '100%'}}>
       <ScrollView>
-        <Text>Hello</Text>
+        <ProductsContentContainer>
+          {products && products.length > 0
+            ? products.map((product: any) => {
+                return (
+                  <TouchableOpacity>
+                    <ProductContentBoxTextContainer>
+                      <ProductsContentOutsideBox>
+                        <ProductsContentBox>
+                          <ProductsContentImages
+                            source={{
+                              uri:
+                                product &&
+                                product.images &&
+                                product.images.length > 0
+                                  ? variables.siteUrl +
+                                    '/api/repo/' +
+                                    product.images[0]
+                                  : undefined,
+                            }}
+                          />
+                          <ProductsContentBoxText>
+                            {product.name}
+                          </ProductsContentBoxText>
+
+                          <ProductsContentBoxSubText>
+                            {product.description_short}
+                          </ProductsContentBoxSubText>
+
+                          <ProductsContentBoxPriceContainer>
+                            <ProductsContentBoxPriceText>
+                              {product.price} ₽
+                            </ProductsContentBoxPriceText>
+                          </ProductsContentBoxPriceContainer>
+                        </ProductsContentBox>
+                      </ProductsContentOutsideBox>
+                    </ProductContentBoxTextContainer>
+                  </TouchableOpacity>
+                );
+              })
+            : undefined}
+        </ProductsContentContainer>
       </ScrollView>
     </View>
   );
 };
 
-export default Categories;
+export default Products;
+
+const ProductsContentContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  padding: 5px 5px 5px 5px;
+  gap: 10px;
+`;
+
+const ProductsContentOutsideBox = styled.View`
+  margin-bottom: 125%;
+`;
+
+const ProductsContentBox = styled.View`
+  background-color: ${variables.COLORS.white};
+
+  width: 115px;
+  height: 110px;
+  border-radius: 8px;
+`;
+
+const ProductsContentImages = styled.Image`
+  object-fit: contain;
+
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+`;
+
+const ProductsContentBoxText = styled.Text`
+  color: ${variables.COLORS.black};
+  font-size: ${variables.SIZES.h9};
+
+  height: 50%;
+  margin-left: 10px;
+  margin-top: 10px;
+`;
+
+const ProductsContentBoxSubText = styled.Text`
+  color: ${variables.COLORS.gray};
+  font-size: ${variables.SIZES.h9};
+
+  margin-left: 10px;
+  margin-top: 15px;
+`;
+
+const ProductsContentBoxPriceContainer = styled.View`
+  background-color: ${variables.COLORS.white};
+  font-size: ${variables.SIZES.h8};
+  margin-top: 15px;
+
+  width: 90px;
+  padding: 5px;
+  border-radius: 5px;
+
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const ProductsContentBoxPriceText = styled.Text`
+  color: ${variables.COLORS.black};
+  font-size: ${variables.SIZES.h8};
+  text-align: center;
+`;
+
+const ProductContentBoxTextContainer = styled.View`
+  background-color: ${variables.COLORS.brightgray};
+
+  border-radius: 8px;
+  margin-bottom: 15px;
+`;
