8aa8677a28d13110f6606870684f2dbaa1ca2235
H3XG0D <99123233+H3XG0D@users.noreply.github.com>
2023-04-07 23:58:44 +0900

    Fixing bugs

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import {LogBox} from 'react-native';
 
 import NavigationRoute from './navigation/NavigationRoute';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 const App = () => {
   LogBox.ignoreLogs(['Warning: ...']);
   LogBox.ignoreAllLogs();
 
   return (
-    <>
+    <SafeAreaProvider>
       <NavigationRoute />
-    </>
+    </SafeAreaProvider>
   );
 };
 
--- navigation/NavigationRoute.tsx
+++ navigation/NavigationRoute.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import 'react-native-gesture-handler';
+
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParams} from '../src/config/routes';
+
 import LoginPage from '../components/Login/Login';
 import Market from '../components/Market/Screens/Main/Market';
 import Forget from '../components/Login/forget-password/Forget';
@@ -26,75 +29,81 @@ const RootStack = createNativeStackNavigator<RootStackParams>();
 
 const NavigationRoute = () => {
   return (
-    <NavigationContainer>
-      {/* Login Screens */}
-      <RootStack.Navigator
-        initialRouteName={'LoadingScreen'}
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerTitleStyle: {fontSize: 24},
-        }}>
-        <RootStack.Screen
-          name="Login"
-          component={LoginPage}
-          options={{headerShown: false}}></RootStack.Screen>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {/* Login Screens */}
+        <RootStack.Navigator
+          initialRouteName={'LoadingScreen'}
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerTitleStyle: {fontSize: 24},
+          }}>
+          <RootStack.Screen
+            name="Login"
+            component={LoginPage}
+            options={{headerShown: false}}></RootStack.Screen>
 
-        <RootStack.Screen name="Forget" component={Forget}></RootStack.Screen>
-        <RootStack.Screen
-          name="ForgetCode"
-          component={ForgetCode}></RootStack.Screen>
+          <RootStack.Screen name="Forget" component={Forget}></RootStack.Screen>
+          <RootStack.Screen
+            name="ForgetCode"
+            component={ForgetCode}></RootStack.Screen>
 
-        <RootStack.Screen name="Code" component={Code}></RootStack.Screen>
+          <RootStack.Screen name="Code" component={Code}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="ResetPassword"
-          component={ResetPassword}></RootStack.Screen>
+          <RootStack.Screen
+            name="ResetPassword"
+            component={ResetPassword}></RootStack.Screen>
 
-        <RootStack.Screen name="Details" component={Details}></RootStack.Screen>
+          <RootStack.Screen
+            name="Details"
+            component={Details}></RootStack.Screen>
 
-        <RootStack.Screen name="Next" component={Next}></RootStack.Screen>
+          <RootStack.Screen name="Next" component={Next}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="Registration"
-          component={Registration}></RootStack.Screen>
+          <RootStack.Screen
+            name="Registration"
+            component={Registration}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="LoadingScreen"
-          component={LoadingScreen}></RootStack.Screen>
+          <RootStack.Screen
+            name="LoadingScreen"
+            component={LoadingScreen}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="MyRequest"
-          component={MyRequest}></RootStack.Screen>
+          <RootStack.Screen
+            name="MyRequest"
+            component={MyRequest}></RootStack.Screen>
 
-        <RootStack.Screen name="Request" component={Request}></RootStack.Screen>
+          <RootStack.Screen
+            name="Request"
+            component={Request}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="UserProfile"
-          component={UserProfile}></RootStack.Screen>
+          <RootStack.Screen
+            name="UserProfile"
+            component={UserProfile}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="Products"
-          component={Products}></RootStack.Screen>
+          <RootStack.Screen
+            name="Products"
+            component={Products}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="Categories"
-          component={Categories}></RootStack.Screen>
+          <RootStack.Screen
+            name="Categories"
+            component={Categories}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="ParentMeat"
-          component={ParentMeat}></RootStack.Screen>
+          <RootStack.Screen
+            name="ParentMeat"
+            component={ParentMeat}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="Supplier"
-          component={Supplier}></RootStack.Screen>
+          <RootStack.Screen
+            name="Supplier"
+            component={Supplier}></RootStack.Screen>
 
-        <RootStack.Screen
-          name="BottomTabNav"
-          component={BottomTabNav}></RootStack.Screen>
+          <RootStack.Screen
+            name="BottomTabNav"
+            component={BottomTabNav}></RootStack.Screen>
 
-        <RootStack.Screen name="Market" component={Market}></RootStack.Screen>
-      </RootStack.Navigator>
-    </NavigationContainer>
+          <RootStack.Screen name="Market" component={Market}></RootStack.Screen>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
