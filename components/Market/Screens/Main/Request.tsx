84c1914b69e928ba4e135c330a4a74b645f4e44e
H3XG0D <99123233+H3XG0D@users.noreply.github.com>
2023-04-12 13:28:27 +0900

    Small commit for cart items

--- components/Market/Screens/Main/Request.tsx
+++ components/Market/Screens/Main/Request.tsx
@@ -34,188 +34,12 @@ const Request = (props: Props) => {
     });
   }, [navigation]);
 
-  // const getProducts = async () => {
-  //   const getProductData = await AsyncStorage.getItem('savedProductData');
-  //   if (getProductData != null) {
-  //     const array = JSON.parse(getProductData);
-  //     setProducts(array.data);
-  //   } else {
-  //     console.log('error');
-  //   }
-  // };
-
-  // const readData = async () => {
-  //   const productCode = await AsyncStorage.getItem('ProductCode');
-  //   const productName = await AsyncStorage.getItem('ProductName');
-  //   const productQuantum = await AsyncStorage.getItem('ProductQuantum');
-  //   const productImage = await AsyncStorage.getItem('ProductImages');
-  //   const productPrice = await AsyncStorage.getItem('ProductPrice');
-  //   const productWeight = await AsyncStorage.getItem('ProductWeight');
-  //   const productFinalCost = await AsyncStorage.getItem('ProductFinalCost');
-
-  //   const productDetail = [
-  //     {
-  //       name: productName,
-  //       code: productCode,
-  //       quantum: productQuantum,
-  //       image: productImage,
-  //       price: productPrice,
-  //       weight: productWeight,
-  //       final: productFinalCost,
-  //     },
-  //   ];
-  //   setProducts(productDetail);
-
-  //   const image = productImage?.replace(/"/g, '');
-  //   setImages(image);
-  // };
-
-  // React.useEffect(() => {
-  //   readData();
-  // }, []);
-
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //     readData();
-  //   }, []),
-  // );
-
   return (
-    <View>
-      <Text>{props.quantity?.count}</Text>
-      {/* <RequestMain style={{elevation: 1}}>
-        <Text>Корзина для покупок</Text>
-      </RequestMain>
-      <FlatList
-        data={[1, 1, 1, 1]}
-        renderItem={({item, index}) => {
-          return (
-            <View
-              style={{
-                width: '93%',
-                alignSelf: 'center',
-                height: 120,
-                backgroundColor: '#FFF',
-                marginTop: 10,
-                borderRadius: 10,
-                elevation: 1,
-                flexDirection: 'row',
-              }}></View>
-          );
-        }}></FlatList> */}
-
-      {/* {products && products.length > 0
-        ? products.map((res: any) => {
-            return (
-              <RequestMain>
-                <View
-                  style={{
-                    width: '93%',
-                    alignItems: 'center',
-                    height: 120,
-                    backgroundColor: '#FFF',
-                    marginTop: 10,
-                    borderRadius: 10,
-                    elevation: 1,
-                    flexDirection: 'row',
-                    alignSelf: 'center',
-                  }}>
-                  <View
-                    style={{
-                      width: 110,
-                      height: 80,
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      borderRadius: 10,
-                      gap: 20,
-                      marginLeft: 20,
-
-                      borderTopWidth: 1,
-                      borderLeftWidth: 1,
-                      borderRightWidth: 1,
-                      borderBottomWidth: 1,
-
-                      borderColor: variables.COLORS.brightgray,
-                    }}>
-                    <Image
-                      source={{uri: images}}
-                      style={{
-                        width: '100%',
-                        height: '100%',
-                        resizeMode: 'contain',
-                        borderRadius: 10,
-                        padding: 5,
-                      }}></Image>
-                    <View>
-                      <Text
-                        numberOfLines={3}
-                        style={{fontSize: variables.SIZES.h8}}>
-                        {JSON.stringify(res.name).replace(/\\|"/g, '')}
-                      </Text>
-                      <Text
-                        style={{
-                          fontSize: variables.SIZES.h9,
-                          color: variables.COLORS.gray,
-                          marginTop: 3,
-                        }}>
-                        {JSON.stringify(res.weight).replace(/\\|"/g, '')}{' '}
-                        <Text style={{color: variables.COLORS.primary}}>
-                          • {JSON.stringify(res.price).replace(/\\|"/g, '')} ₽
-                        </Text>
-                      </Text>
-                    </View>
-                  </View>
-                  <View
-                    style={{
-                      marginLeft: 'auto',
-                      marginBottom: 'auto',
-                      marginRight: 5,
-                      marginTop: 20,
-                    }}>
-                    <View>
-                      <Text
-                        style={{
-                          color: variables.COLORS.primary,
-                          fontSize: variables.SIZES.h9,
-                          fontWeight: '600',
-                          marginLeft: 'auto',
-                        }}>
-                        {JSON.stringify(res.final).replace(/\\|"/g, '')} ₽
-                      </Text>
-                      <View
-                        style={{
-                          alignItems: 'center',
-                          marginTop: 15,
-                          gap: 10,
-                          flexDirection: 'row',
-                        }}>
-                        <TouchableOpacity>
-                          <MiniBtnView>
-                            <MiniBtnText>-</MiniBtnText>
-                          </MiniBtnView>
-                        </TouchableOpacity>
-                        <Text>
-                          {JSON.stringify(res.quantum).replace(/\\|"/g, '')}
-                        </Text>
-                        <TouchableOpacity>
-                          <MiniBtnView>
-                            <MiniBtnText>+</MiniBtnText>
-                          </MiniBtnView>
-                        </TouchableOpacity>
-                      </View>
-                    </View>
-                  </View>
-                </View>
-                <TouchableOpacity>
-                  <PaymentButtonView>
-                    <PaymentButtonText>Оформить заявку</PaymentButtonText>
-                  </PaymentButtonView>
-                </TouchableOpacity>
-              </RequestMain>
-            );
-          })
-        : null} */}
-    </View>
+    <RequestMain>
+      <Text style={{textAlign: 'center'}}>
+        Quantity of products: {props.quantity?.count}
+      </Text>
+    </RequestMain>
   );
 };
 
@@ -223,6 +47,11 @@ export default Request;
 
 const RequestMain = styled.View`
   width: 100%;
+  height: 100%;
+
+  align-items: center;
+  justify-content: center;
+
   background-color: ${variables.COLORS.white};
 `;
 
